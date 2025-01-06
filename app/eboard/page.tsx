3ae795045f9326957bc877d42ae5726e4a4befb2
d5vis <davisjbanks@gmail.com>
2025-01-06 00:12:29 -0800

    add profile pics and linkedins

--- app/eboard/page.tsx
+++ app/eboard/page.tsx
@@ -5,61 +5,71 @@ const members = [
     name: "Adeolu (Danny) Adebiyi",
     position: "Co-President",
     major: "Computer Engineering",
-    image: "/images/profiles/danny.png",
+    image: "/images/profiles/danny.jpeg",
+    linkedInUrl: "https://www.linkedin.com/in/adeolu-adebiyi/",
   },
   {
     name: "Davis Banks",
     position: "Co-President",
     major: "Computer Science",
-    image: "/images/profiles/davis.png",
+    image: "/images/profiles/davis.jpeg",
+    linkedInUrl: "https://www.linkedin.com/in/d5vis",
   },
   {
     name: "Makari Green",
     position: "Vice President",
     major: "Computer Engineering",
-    image: "/images/profiles/makari.png",
+    image: "/images/profiles/makari.jpeg",
+    linkedInUrl: "https://www.linkedin.com/in/makgreen2820",
   },
   {
     name: "Dawit Solomon",
     position: "Secretary",
     major: "Computer Science",
-    image: "/images/profiles/dawit.png",
+    image: "/images/profiles/dawit.jpeg",
+    linkedInUrl: "https://www.linkedin.com/in/dawitsol",
   },
   {
     name: "Tinbite Ermias",
     position: "Treasurer",
     major: "Computer Engineering",
-    image: "/images/profiles/tinbite.png",
+    image: "/images/profiles/tinbite.jpeg",
+    linkedInUrl: "https://www.linkedin.com/in/tinbite-ermias/",
   },
   {
     name: "Brandon Bazile",
     position: "Co-Media and Marketing Chair",
     major: "Computer Science",
-    image: "/images/profiles/brandon.png",
+    image: "/images/profiles/brandon.jpeg",
+    linkedInUrl: "https://www.linkedin.com/in/brandon-bazile-hgp/",
   },
   {
     name: "Sarah Leka",
     position: "Co-Media and Marketing Chair",
     major: "Mechanical Engineering",
-    image: "/images/profiles/sarah.png",
+    image: "/images/profiles/sarah.jpeg",
+    linkedInUrl: "https://www.linkedin.com/in/sarah-leka-354a5a2a7/",
   },
   {
     name: "Jordan Harvey",
     position: "Programs Chair",
     major: "Mechanical Engineering",
-    image: "/images/profiles/jordan.png",
+    image: "/images/profiles/jordan.jpeg",
+    linkedInUrl: "https://www.linkedin.com/in/jordanharvey-/",
   },
   {
     name: "Natasha Cordova-Diba",
     position: "Senator",
     major: "Computer Science",
-    image: "/images/profiles/natasha.png",
+    image: "/images/profiles/natasha.jpeg",
+    linkedInUrl: "https://www.linkedin.com/in/natasha-cordova-diba/",
   },
   {
     name: "Hannah Holden",
     position: "Senator",
     major: "Computer Science",
-    image: "/images/profiles/hannah.png",
+    image: "/images/profiles/hannah.jpeg",
+    linkedInUrl: "https://www.linkedin.com/in/hannah-v-h/",
   },
 ];
 
@@ -74,6 +84,7 @@ const Eboard = () => {
           position={member.position}
           major={member.major}
           image={member.image}
+          linkedInUrl={member.linkedInUrl}
         />
       ))}
     </div>
--- components/profile.tsx
+++ components/profile.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface ProfileProps {
   index: number;
@@ -6,6 +7,7 @@ interface ProfileProps {
   position: string;
   major: string;
   image: string;
+  linkedInUrl: string;
 }
 
 const Profile: React.FC<ProfileProps> = ({
@@ -14,6 +16,7 @@ const Profile: React.FC<ProfileProps> = ({
   position,
   major,
   image,
+  linkedInUrl = "",
 }) => {
   return (
     <div className="grid grid-cols-2 gap-4">
@@ -22,12 +25,25 @@ const Profile: React.FC<ProfileProps> = ({
         alt={name}
         width={500}
         height={500}
-        className={`motion-blur-in motion-delay-${index * 100} bg-gray-100`}
+        className={`motion-blur-in motion-delay-[${index * 100}ms] bg-gray-100`}
       />
       <div className="flex flex-col gap-2">
         <h2 className="font-bold text-md">{name}</h2>
         <p className="text-md text-gray-500">{position}</p>
         <p className="text-sm text-gray-500">{major}</p>
+        <Link href={linkedInUrl} target="_blank">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="24"
+            height="24"
+            viewBox="0 0 50 50"
+            className="fill-gray-500 hover:fill-gray-700 transition-all"
+          >
+            <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path>
+          </svg>
+        </Link>
       </div>
     </div>
   );
